f101e0189bd18bf2c1cc17dba7ce8d76c79a0e04
syrene <144994323+shb1383@users.noreply.github.com>
2024-09-24 18:41:26 +0900

    Initial Commit

---
+++ VIewsAndModifiers/VIewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIewsAndModifiersApp.swift
+//  VIewsAndModifiers
+//
+//  Created by Syrene Haber Bartolome on 2024/09/24.
+//
+
+import SwiftUI
+
+@main
+struct VIewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VIewsAndModifiersTests/VIewsAndModifiersTests.swift
@@ -0,0 +1,17 @@
+//
+//  VIewsAndModifiersTests.swift
+//  VIewsAndModifiersTests
+//
+//  Created by Syrene Haber Bartolome on 2024/09/24.
+//
+
+import Testing
+@testable import VIewsAndModifiers
+
+struct VIewsAndModifiersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
